f77c3665af6d83094d7717969e602438e916b759
haeun <yoohaaeun@gmail.com>
2023-01-30 17:01:23 +0900

    비디오 카드(목데이터)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet } from 'react-router-dom';
 import SearchHeader from './components/SearchHeader';
 
+const queryClient = new QueryClient();
 function App() {
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
