a82d7ef4da1497cc45e68af65e3871ad9cbb49fe
haeun <yoohaaeun@gmail.com>
2023-01-30 15:30:17 +0900

    검색 헤더 기능

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function Videos() {
-  return <>Videos</>;
+  const { keyword } = useParams();
+  return <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>;
 }
