df0a8948c4783a9eeebc73c431ba40032093ddc8
haeun <yoohaaeun@gmail.com>
2023-01-30 17:13:38 +0900

    비디오 카드 (axios 라이브러리)

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -3,6 +3,7 @@ import { findAllByAltText } from '@testing-library/react';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import VideoCard from '../components/VideoCard';
+import axios from 'axios';
 
 export default function Videos() {
   const { keyword } = useParams();
@@ -11,9 +12,9 @@ export default function Videos() {
     error,
     data: videos,
   } = useQuery(['videos', keyword], async () => {
-    return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
-      .then((res) => res.json())
-      .then((data) => data.items);
+    return axios
+      .get(`/videos/${keyword ? 'search' : 'popular'}.json`)
+      .then((res) => res.data.items);
   });
 
   return (
