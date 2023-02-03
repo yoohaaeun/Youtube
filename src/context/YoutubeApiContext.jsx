6a2aed3cb46dbc61ef10e1a35203d456ecc7829e
haeun <yoohaaeun@gmail.com>
2023-02-03 17:15:09 +0900

    비디오 카드 (최종 리팩토링)

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,11 +1,13 @@
 import { createContext, useContext } from 'react';
-import FakeYoutube from '../api/fakeYoutube';
+// import FakeYoutubeClient from '../api/fakeYoutubeClient';
 import Youtube from '../api/youtube';
+import YoutubeClient from '../api/youtubeClient';
 
 export const YoutubeApiContext = createContext();
 
-const youtube = new Youtube();
-// const youtube = new FakeYoutube(); // mock데이터 사용시
+// const client = new FakeYoutubeClient();
+const client = new YoutubeClient();
+const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
   return (
